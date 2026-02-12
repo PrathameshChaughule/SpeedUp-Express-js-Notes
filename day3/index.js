4e404eeac7cd9296a3231d40ac648a3b61cd59d1
PrathameshChaughule <pvc14102002@gmail.com>
2026-02-12 12:25:18 +0530

    day4

--- day3/index.js
+++ day3/index.js
@@ -16,6 +16,10 @@ app.get('/about', (req, res) => {
     res.send(`<h1>ABOUT</h1><img src="sl.png" alt="Solo Leveling"/>`)
 })
 
+app.use((req, res) => {
+    res.send('<h1>404 Page not Found</h1>')
+})
+
 const HOST = "127.0.0.1"
 const PORT = 3000
 app.listen(PORT, HOST, () => {

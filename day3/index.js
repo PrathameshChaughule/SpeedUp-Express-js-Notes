70b4175d6110e617bc4bbb0dc1d0e18fade2256b
PrathameshChaughule <pvc14102002@gmail.com>
2026-02-12 10:15:42 +0530

    day3

---
+++ day3/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static("public/"))
+
+app.get('/', (req, res) => {
+    // res.send("Hiii")
+    let obj = {
+        name: "Prathamesh",
+        rollNo: 21
+    }
+    res.render('home.ejs', { data: obj })
+})
+
+app.get('/about', (req, res) => {
+    res.send(`<h1>ABOUT</h1><img src="sl.png" alt="Solo Leveling"/>`)
+})
+
+const HOST = "127.0.0.1"
+const PORT = 3000
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+})
